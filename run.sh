feea0e50bbee8795e36fbe8d88a9f1431043e7fb
lee-orr <leeorr@gmail.com>
2022-05-31 15:08:20 +0000

    better initial run

---
+++ run.sh
@@ -0,0 +1,10 @@
+if ! trunk -v trunk &> /dev/null
+then
+    echo "trunk doesn't exist"
+    sudo apt-get update
+    sudo apt-get install --no-install-recommends libasound2-dev libudev-dev
+    rustup target add wasm32-unknown-unknown
+    cargo install trunk
+fi
+
+trunk serve
