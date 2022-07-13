eca682119ef91b8344024ebb09f2f873b32695f1
lee-orr <leeorr@gmail.com>
2022-07-13 17:23:00 +0000

    adjust settings

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-URL="${GITPOD_WORKSPACE_URL/https:\/\//""}"    
+URL="match.gschup.dev/bevy"
+# ${GITPOD_WORKSPACE_URL/https:\/\//""}"    
 echo "{\"signaling_url\":\"$URL\"}" > ./assets/settings.json
 matchbox_server & trunk serve --proxy-backend http://localhost:3538 --proxy-rewrite /server/ --proxy-ws && fg
